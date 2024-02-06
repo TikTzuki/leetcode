ace6d5ba45e9c65b1664c0e76d463a0c52565b07
tiktuzki <tiktuzki@gmail.com>
2024-02-06 20:28:19 +0700

    :))

--- src/main/java/org/tiktzuki/leetcode/easy/ValidAnagram.java
+++ src/main/java/org/tiktzuki/leetcode/easy/ValidAnagram.java
@@ -1,22 +1,25 @@
 package org.tiktzuki.leetcode.easy;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class ValidAnagram {
+    Set<Map<Integer, Integer>> sets = new HashSet<>();
+
     public boolean isAnagram(String s, String t) {
-        Map<Integer, Integer> map = new HashMap<>();
-        s.chars().forEach(c -> {
-            map.merge(c, 1, Integer::sum);
-        });
-        t.chars().forEach(c -> {
-            int count = map.getOrDefault(c, 0) - 1;
-            map.put(c, count);
-        });
-        return map.values().stream().allMatch(x->x.equals(0));
+        Map<Integer, Integer> m = new HashMap<>();
+        s.chars().forEach(c -> m.merge(c, 1, Integer::sum));
+        sets.add(m);
+
+        Map<Integer, Integer> m2 = new HashMap<>();
+        t.chars().forEach(c -> m2.merge(c, 1, Integer::sum));
+        return sets.contains(m2);
     }
+
     public static void main(String[] args) {
-        String s = "rat";
+        String s = "rac";
         String t = "car";
         boolean rs = new ValidAnagram().isAnagram(s, t);
         System.out.println(rs);
--- src/main/java/org/tiktzuki/leetcode/medium/GroupAnagrams.java
+++ src/main/java/org/tiktzuki/leetcode/medium/GroupAnagrams.java
@@ -1,30 +1,45 @@
 package org.tiktzuki.leetcode.medium;
 
 import java.util.*;
-import java.util.function.IntConsumer;
-import java.util.stream.IntStream;
 
 public class GroupAnagrams {
-    private int hasCode(String s){
-        return s.chars().sum();
+    Set<Map<Integer, Integer>> sets = new HashSet<>();
+
+    public boolean isAnagram(String s, String t) {
+        Map<Integer, Integer> m = new HashMap<>();
+        s.chars().forEach(c -> m.merge(c, 1, Integer::sum));
+        sets.add(m);
+
+        Map<Integer, Integer> m2 = new HashMap<>();
+        t.chars().forEach(c -> m2.merge(c, 1, Integer::sum));
+        return sets.contains(m2);
     }
+
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> rs = new ArrayList<>();
-        Map<Integer, List<String>> map = new HashMap<>();
-        for(String s: strs){
-            int key = s.chars().reduce(1, (left, right) -> {
-               return left*right;
-            });
-            List<String> value = map.getOrDefault(key, new ArrayList<>());
-            value.add(s);
-            map.put(key, value);
+        List<List<String>> anagrams = new ArrayList<>();
+        for (int i = 0; i < strs.length; i++) {
+            String first = strs[i];
+            if (first == null)
+                continue;
+            List<String> anagram = new ArrayList<>();
+            anagram.add(first);
+            for (int j = i + 1; j < strs.length; j++) {
+                String sec = strs[j];
+                if (sec == null)
+                    continue;
+                if (isAnagram(first, sec)) {
+                    strs[j] = null;
+                    anagram.add(sec);
+                }
+            }
+            anagrams.add(anagram);
         }
-        return map.values().stream().toList();
+        return anagrams;
     }
 
     public static void main(String[] args) {
-        var algorithm  = new GroupAnagrams();
-        var input = new String[]{"eat","tea","tan","ate","nat","bat"};
+        var algorithm = new GroupAnagrams();
+        var input = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
         var output = algorithm.groupAnagrams(input);
         output.forEach(System.out::println);
     }
